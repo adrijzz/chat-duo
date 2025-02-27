752855b6554cd5b74fabb51a782bdb19d42a7055
adrijzz <adriancosta0747@gmail.com>
2025-02-27 19:53:33 -0300

    chore: add vercel and npm configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/chat-duo/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
   server: {
     host: true, // Permite acesso de outros dispositivos na rede
     port: 5173
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   }
 });
